2f4a3fd34fa705935505aa06defcc659f0453ae8
FRC Team 5826 AvisAutomata <frc5826@gmail.com>
2025-02-04 17:53:06 -0600

    Added a way to access buttons from Network Tables

---
+++ src/main/java/frc/robot/subsystems/ButtonBoard.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.BooleanSubscriber;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.PubSubOption;
+
+public class ButtonBoard {
+
+    private BooleanSubscriber[] buttons;
+
+    public ButtonBoard(int totalButtons) {
+        buttons = new BooleanSubscriber[totalButtons];
+        NetworkTableInstance instance = NetworkTableInstance.getDefault();
+        for(int i = 0; i < totalButtons; i++){
+            buttons[i] = instance.getBooleanTopic("buttons/" + i).subscribe(false, PubSubOption.hidden(false));
+        }
+    }
+
+    public boolean getButton(int button){
+        if (button < buttons.length && button >= 0) {
+            return buttons[button].get();
+        }
+        else {
+            System.err.println("Button " + button + " does not exist!");
+            return false;
+        }
+    }
+
+}
