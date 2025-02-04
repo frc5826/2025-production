47430145e42bb025c2fd868ca5ed4ada594130fa
FRC Team 5826 AvisAutomata <frc5826@gmail.com>
2025-02-04 16:48:57 -0600

    Added Lidar and Ultrasonic to Production

--- src/main/java/frc/robot/sensors/LidarPWM.java
+++ src/main/java/frc/robot/sensors/LidarPWM.java
@@ -2,11 +2,49 @@ package frc.robot.sensors;
 
 import edu.wpi.first.networktables.NTSendable;
 import edu.wpi.first.networktables.NTSendableBuilder;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.DutyCycle;
 
-//TODO
+
 public class LidarPWM implements NTSendable {
+
+    public DutyCycle lidarReading;
+    public DigitalOutput lidarTrigger;
+
+    public LidarPWM(int triggerChannel, int readingChannel){
+
+        lidarTrigger = new DigitalOutput(triggerChannel);
+        lidarReading = new DutyCycle(new DigitalInput(readingChannel));
+
+    }
+
+    public void turnOn(){
+        lidarTrigger.set(false);
+    }
+
+    public void turnOff(){
+        lidarTrigger.set(true);
+    }
+
+    /**
+     * @return Measurement in meters
+     */
+    public double getMeasurement() {
+
+        return lidarReading.getHighTimeNanoseconds()/100000.0;
+
+    }
+
+    public boolean isOn(){
+        return !lidarTrigger.get();
+    }
+
     @Override
     public void initSendable(NTSendableBuilder builder) {
-        //TODO Add properties to builder
+        builder.setSmartDashboardType("5826-LidarPWM");
+        builder.addDoubleProperty("Measurment M", this::getMeasurement, null);
+        builder.addBooleanProperty("Is Sensor On?", this::isOn, null);
+
     }
 }
--- src/main/java/frc/robot/sensors/UltrasonicPWM.java
+++ src/main/java/frc/robot/sensors/UltrasonicPWM.java
@@ -2,11 +2,32 @@ package frc.robot.sensors;
 
 import edu.wpi.first.networktables.NTSendable;
 import edu.wpi.first.networktables.NTSendableBuilder;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycle;
 
 //TODO
 public class UltrasonicPWM implements NTSendable {
+
+
+    private DutyCycle ultrasonicReading;
+
+
+    public UltrasonicPWM(int channel){
+        ultrasonicReading = new DutyCycle(new DigitalInput(channel));
+    }
+
+    /**
+     * @return Reading in Meters
+     */
+    public double getUltrasonic(){
+        return ultrasonicReading.getHighTimeNanoseconds()/100000.0;
+    }
+
+
     @Override
     public void initSendable(NTSendableBuilder builder) {
-        //TODO Add properties to builder
+
+        builder.setSmartDashboardType("5826-Ultrasonic");
+        builder.addDoubleProperty("Millimeters", this::getUltrasonic, null);
     }
 }
