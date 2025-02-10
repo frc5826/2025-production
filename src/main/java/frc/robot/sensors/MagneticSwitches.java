310f087a9f477affac1bd727fe3f082c5c9721f0
FRC Team 5826 AvisAutomata <frc5826@gmail.com>
2025-02-10 17:16:35 -0600

    Update MagneticSwitches.java

--- src/main/java/frc/robot/sensors/MagneticSwitches.java
+++ src/main/java/frc/robot/sensors/MagneticSwitches.java
@@ -1,17 +1,26 @@
 package frc.robot.sensors;
 
+import edu.wpi.first.networktables.NTSendable;
+import edu.wpi.first.networktables.NTSendableBuilder;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DutyCycle;
 
-public class MagneticSwitches {
+public class MagneticSwitches implements NTSendable {
 
-//    private DutyCycle magneticSwitch1, magneticSwitch2, magneticSwitch3;
-//    public MagneticSwitches(int channel1, int channel2, int channel3){
-//        magneticSwitch1 = new DutyCycle(new DigitalInput(channel1));
-//        magneticSwitch2 = new DutyCycle(new DigitalInput(channel2));
-//        magneticSwitch3 = new DutyCycle(new DigitalInput(channel3));
-//    }
-//
-//    public boolean isTriggered()
-//    }
+    private DigitalInput limitSwitch1, limitSwitch2, limitSwitch3;
+
+    public MagneticSwitches(int channel1, int channel2, int channel3){
+        limitSwitch1 = new DigitalInput(channel1);
+        limitSwitch2 = new DigitalInput(channel2);
+        limitSwitch3 = new DigitalInput(channel3);
+    }
+
+    public boolean getMagSwitch(){
+        return limitSwitch1.get() && limitSwitch2.get() && limitSwitch3.get();
+    }
+
+    @Override
+    public void initSendable(NTSendableBuilder builder) {
+        builder.setSmartDashboardType("5826-MagSwitches");
+        builder.addBooleanProperty("All Switched?", this::getMagSwitch, null);
+    }
 }
